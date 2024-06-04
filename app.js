8e1b02c8793333d1af3ac2826e13e812affe4454
Guilherme <guilhermesebastiaou.u@gmail.com>
2024-06-04 12:13:36 -0300

    alteracao

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
-const TestRouter = require("./routes/TestRouter");
-const ContactRouter = require("./routes/ContactRouter");
-const CvDownloadRouter = require("./routes/CvDownloadRouter");
+const TestRouter = require("./src/routes/TestRouter");
+const ContactRouter = require("./src/routes/ContactRouter");
+const CvDownloadRouter = require("./src/routes/CvDownloadRouter");
 
 class App {
   constructor(){
